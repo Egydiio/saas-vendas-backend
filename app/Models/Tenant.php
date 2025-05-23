c394362447d70d60188f11fafcab1b08886fe74c
Egydio <egydiio@hotmail.com>
2025-05-23 18:15:19 -0300

    adding Tenants

---
+++ app/Models/Tenant.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\TenantFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tenant extends Model
+{
+    /** @use HasFactory<TenantFactory> */
+    use HasFactory;
+
+    protected $fillable =[
+        'name',
+    ];
+}
