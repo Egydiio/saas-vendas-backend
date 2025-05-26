23e2838aa12ac5177af6fb786fe65ca2239433b0
Egydio <egydiio@hotmail.com>
2025-05-26 14:43:53 -0300

    adding Product

---
+++ database/migrations/2025_05_26_172835_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid();
+            $table->foreignId('tenant_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->integer('price')->nullable()->unsigned();
+            $table->integer('stock')->nullable()->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
